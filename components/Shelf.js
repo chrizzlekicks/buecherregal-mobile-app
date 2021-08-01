b1359d625de5e205472bf5f58f4b8ae9e4278e9c
Chris Schimetschka <chris.schimetschka@gmail.com>
2021-08-01 11:23:46 +0200

    uploadbook feat

--- components/Shelf.js
+++ components/Shelf.js
@@ -1,24 +1,18 @@
-import React, {useState } from "react"
-import { View, Image, Text } from "react-native"
+import React, { useState } from 'react'
+import { View, Image, Text } from 'react-native'
 
-import Book from "./Book"
+import Book from './Book'
 
+function Shelf() {
+  const [bookData, setBookData] = useState([
+    { _id: 12345678, name: 'name1', author: 'author1', category: 'category1' },
+    { _id: 23456789, name: 'name2', author: 'author2', category: 'category2' },
+    { _id: 34567890, name: 'name3', author: 'author3', category: 'category2' },
+    { _id: 45678901, name: 'name4', author: 'author4', category: 'category1' },
+    { _id: 56789012, name: 'name5', author: 'author5', category: 'category3' },
+  ])
 
-
-function Shelf(){
-
-    const [bookData, setBookData] = useState([
-        {_id: 12345678, name: "name1", author: "author1", category: "category1" },
-        {_id: 23456789, name: "name2", author: "author2", category: "category2" },
-        {_id: 34567890, name: "name3", author: "author3", category: "category2" },
-        {_id: 45678901, name: "name4", author: "author4", category: "category1" },
-        {_id: 56789012, name: "name5", author: "author5", category: "category3" },
-    ])
-
-    return(
-        <>
-        </>
-    )
+  return <></>
 }
 
 export default Shelf
--- pages/UploadBooks.js
+++ pages/UploadBooks.js
@@ -1,12 +1,107 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import {
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Button,
+  View,
+} from 'react-native'
 
 const UploadBooks = () => {
+  const [newBook, setNewBook] = useState({
+    title: '',
+    author: '',
+    genre: '',
+    language: '',
+    condition: '',
+    desc: '',
+  })
+
+  const deleteInput = () => {
+    setNewBook({
+      title: '',
+      author: '',
+      genre: '',
+      language: '',
+      condition: '',
+      desc: '',
+    })
+  }
+
   return (
-    <View>
-      <Text>Upload Books</Text>
+    <View style={styles.wrapper}>
+      <Text style={styles.title}>Lade hier dein Buch hoch</Text>
+      <TouchableOpacity style={styles.imageUpload}></TouchableOpacity>
+      <TextInput
+        style={styles.input}
+        placeholder='Titel des Buches'
+        onChange={(e) => setNewBook({ ...newBook, title: e.target.value })}
+        value={newBook.title}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='AutorIn des Buches'
+        onChange={(e) => setNewBook({ ...newBook, author: e.target.value })}
+        value={newBook.author}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Genre des Buches'
+        onChange={(e) => setNewBook({ ...newBook, genre: e.target.value })}
+        value={newBook.genre}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Sprache des Buches'
+        onChange={(e) => setNewBook({ ...newBook, language: e.target.value })}
+        value={newBook.language}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Zustand des Buches'
+        onChange={(e) => setNewBook({ ...newBook, condition: e.target.value })}
+        value={newBook.condition}
+      />
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>Hochladen</Text>
+      </TouchableOpacity>
+      <Button onPress={deleteInput} title='Eingaben löschen' color='#ccc' />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'space-around',
+    paddingHorizontal: 10,
+  },
+  title: {
+    textAlign: 'center',
+  },
+  imageUpload: {
+    borderRadius: 50 / 2,
+    width: 50,
+    height: 50,
+    backgroundColor: '#b00055',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    marginVertical: 10,
+  },
+  btn: {
+    alignItems: 'center',
+    backgroundColor: '#b00055',
+    padding: 10,
+  },
+  btnText: {
+    color: '#fff',
+  },
+})
 
 export default UploadBooks
